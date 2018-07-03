df3d44d20b33143cb71b2c416b173311eb2e9a16
xueyanan <2737395562@qq.com>
2018-07-03 11:15:05 +0800

    添加 listview显示下载文件列表

---
+++ app/src/main/java/com/xyn/imooc/FileListAdapter.java
@@ -0,0 +1,90 @@
+package com.xyn.imooc;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.xyn.imooc.entry.FileInfo;
+import com.xyn.imooc.service.DownLoadService;
+
+import java.util.List;
+
+public class FileListAdapter extends BaseAdapter {
+
+    private Context mContext;
+    private List<FileInfo> mFileList;
+
+    public FileListAdapter(Context mContext, List<FileInfo> mFileList) {
+        super();
+        this.mContext = mContext;
+        this.mFileList = mFileList;
+    }
+
+    @Override
+    public int getCount() {
+        return mFileList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mFileList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View view, ViewGroup viewGroup) {
+        ViewHolder holder = null;
+        if (view == null) {
+            view = LayoutInflater.from(mContext).inflate(R.layout.list_item, null);
+            holder = new ViewHolder();
+            holder.tvFile = view.findViewById(R.id.tvfileName);
+            holder.btStop = view.findViewById(R.id.btStop);
+            holder.btStart = view.findViewById(R.id.btStart);
+            holder.pbFile = view.findViewById(R.id.pbProgress);
+            view.setTag(holder);
+        } else {
+            holder = (ViewHolder) view.getTag();
+        }
+        final FileInfo fileInfo = mFileList.get(position);
+        holder.tvFile.setText(fileInfo.getFileName());
+        holder.pbFile.setMax(100);
+        holder.pbFile.setProgress(fileInfo.getFinished());
+        holder.btStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mContext, DownLoadService.class);
+                intent.setAction(DownLoadService.ACTION_START);
+                intent.putExtra("fileInfo", fileInfo);
+                mContext.startService(intent);
+            }
+        });
+
+        holder.btStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mContext, DownLoadService.class);
+                intent.setAction(DownLoadService.ACTION_STOP);
+                intent.putExtra("fileInfo", fileInfo);
+                mContext.startService(intent);
+            }
+        });
+        return view;
+    }
+
+    static class ViewHolder {
+        TextView tvFile;
+        Button btStop;
+        Button btStart;
+        ProgressBar pbFile;
+    }
+}
--- app/src/main/java/com/xyn/imooc/MainActivity.java
+++ app/src/main/java/com/xyn/imooc/MainActivity.java
@@ -8,72 +8,69 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.xyn.imooc.entry.FileInfo;
 import com.xyn.imooc.service.DownLoadService;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTvFileName = null;
-    private ProgressBar mPbProgress = null;
-    private Button mBtStop = null;
-    private Button mBtStart = null;
+    private ListView mLvFile = null;
+    private List<FileInfo> mFileList = null;
+    private FileListAdapter mAdapter = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mTvFileName = findViewById(R.id.tvfileName);
-        mPbProgress = findViewById(R.id.pbProgress);
-        mBtStop = findViewById(R.id.btStop);
-        mBtStart = findViewById(R.id.btStart);
+        mLvFile = findViewById(R.id.lvFile);
 
-        mPbProgress.setMax(100);
-        final FileInfo fileInfo = new FileInfo
-                (0, "http://dlsw.baidu.com/sw-search-sp/soft/1a/11798/kugou_V7.6.85.17344_setup.1427079848.exe",
-                        "kugou_V7.6.85.17344_setup.1427079848.exe", 0, 0);
+        mFileList = new ArrayList<>();
+        //http://www.imooc.com/mobile/imooc.apk
+        //http://www.imooc.com/download/Activator.exe
+        //http://www.imooc.com/download/Itunes64Setup.exe
+        //http://www.imooc.com/download/BaiduPlayerNetSetup_100.exe
+        //http://dlsw.baidu.com/sw-search-sp/soft/1a/11798/kugou_V7.6.85.17344_setup.1427079848.exe
 
-        mTvFileName.setText(fileInfo.getFileName());
-        mBtStart.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, DownLoadService.class);
-                intent.setAction(DownLoadService.ACTION_START);
-                intent.putExtra("fileInfo", fileInfo);
-                startService(intent);
-            }
-        });
+        FileInfo fileInfo = new FileInfo(0, "http://dlsw.baidu.com/sw-search-sp/soft/1a/11798/kugou_V7.6.85.17344_setup.1427079848.exe"
+                , "kugou_V7.6.85.17344_setup.1427079848.exe", 0, 0);
+        FileInfo fileInfo1 = new FileInfo(0, "http://dlsw.baidu.com/sw-search-sp/soft/1a/11798/kugou_V7.6.85.17344_setup.1427079848.exe"
+                , "kugou_V7.6.85.17344_setup.exe", 0, 0);
+        FileInfo fileInfo2 = new FileInfo(0, "http://dlsw.baidu.com/sw-search-sp/soft/1a/11798/kugou_V7.6.85.17344_setup.1427079848.exe"
+                , "kugou_V7.6.85.17344.exe", 0, 0);
+        FileInfo fileInfo3 = new FileInfo(0, "http://dlsw.baidu.com/sw-search-sp/soft/1a/11798/kugou_V7.6.85.17344_setup.1427079848.exe"
+                , "kugou.exe", 0, 0);
 
-        mBtStop.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, DownLoadService.class);
-                intent.setAction(DownLoadService.ACTION_STOP);
-                intent.putExtra("fileInfo", fileInfo);
-                startService(intent);
-            }
-        });
+        mFileList.add(fileInfo);
+        mFileList.add(fileInfo1);
+        mFileList.add(fileInfo2);
+        mFileList.add(fileInfo3);
+        mAdapter = new FileListAdapter(this, mFileList);
+        mLvFile.setAdapter(mAdapter);
 
-        IntentFilter filter = new IntentFilter();
+        /*IntentFilter filter = new IntentFilter();
         filter.addAction(DownLoadService.ACTION_UPDATE);
-        registerReceiver(mReceiver, filter);
+        registerReceiver(mReceiver, filter);*/
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        unregisterReceiver(mReceiver);
+//        unregisterReceiver(mReceiver);
     }
 
-    BroadcastReceiver mReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            if (DownLoadService.ACTION_UPDATE.equals(intent.getAction())) {
-                int finished = intent.getIntExtra("finished", 0);
-                mPbProgress.setProgress(finished);
-            }
-        }
-    };
+//    BroadcastReceiver mReceiver = new BroadcastReceiver() {
+//        @Override
+//        public void onReceive(Context context, Intent intent) {
+//            if (DownLoadService.ACTION_UPDATE.equals(intent.getAction())) {
+//                int finished = intent.getIntExtra("finished", 0);
+//                mPbProgress.setProgress(finished);
+//            }
+//        }
+//    };
 }
