d5b9c07abde443f3ea761fb73c4fe1944ad6d7bb
xueyanan <2737395562@qq.com>
2018-07-03 15:38:08 +0800

    完成通知显示的方法

--- app/src/main/java/com/xyn/imooc/utils/NotificationUtil.java
+++ app/src/main/java/com/xyn/imooc/utils/NotificationUtil.java
@@ -6,10 +6,12 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.widget.RemoteViews;
 
 import com.xyn.imooc.MainActivity;
 import com.xyn.imooc.R;
 import com.xyn.imooc.entry.FileInfo;
+import com.xyn.imooc.service.DownLoadService;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -39,6 +41,25 @@ public class NotificationUtil {
             Intent intent = new Intent(mContext, MainActivity.class);
             PendingIntent pendingIntent = PendingIntent.getActivity(mContext, 0, intent, 0);
             notification.contentIntent = pendingIntent;
+
+            RemoteViews remoteViews = new RemoteViews(mContext.getPackageName(), R.layout.notification);
+
+            Intent intentStart = new Intent(mContext, DownLoadService.class);
+            intentStart.setAction(DownLoadService.ACTION_START);
+            intentStart.putExtra("fileInfo", fileInfo);
+            PendingIntent piStart = PendingIntent.getService(mContext, 0, intentStart, 0);
+            remoteViews.setOnClickPendingIntent(R.id.btStart, piStart);
+
+            Intent intentStop = new Intent(mContext, DownLoadService.class);
+            intentStop.setAction(DownLoadService.ACTION_STOP);
+            intentStop.putExtra("fileInfo", fileInfo);
+            PendingIntent piStop = PendingIntent.getService(mContext, 0, intentStop, 0);
+            remoteViews.setOnClickPendingIntent(R.id.btStop, piStop);
+
+            notification.contentView = remoteViews;
+
+            mNotificationManager.notify(fileInfo.getId(), notification);
+            mNotifications.put(fileInfo.getId(), notification);
         }
     }
 }
